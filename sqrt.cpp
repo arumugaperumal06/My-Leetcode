444fa185241ac0ce50fd902872d4f801304571d1
arumugaperumal06 <perumalarumuga67@gmail.com>
2025-08-04 23:47:02 +0530

    Create sqrt.cpp

---
+++ sqrt.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        if(x==0 || x==1)
+            return x;
+        int low =0;int high =x,ans=0;
+
+        while(low<=high)
+        {
+            long long mid = (low+high)/2;
+            long long sq = mid*mid;
+
+            if(sq==x)
+                return mid;
+            else if(sq<=x)
+            {
+                ans = mid;
+                low = mid+1;
+            }
+            else
+            {
+                high = mid -1;
+            }
+
+        }
+        return ans;
+    }
+};
